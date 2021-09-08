8c0c24a12d7d6cd4a9ae0a2eb8af5653ff5f5e9a
SofieSkovgaard <s184329@student.dtu.dk>
2021-09-08 12:33:59 +0200

    Data prep initiated

---
+++ R/03_data_prep.R
@@ -0,0 +1,29 @@
+# 03 Data preparation for IBM
+
+rm(list = ls())
+
+library(tidyverse)
+
+
+# Import functions --------------------------------------------------------
+source(file ="R/99_functions.R")
+
+# Load data ---------------------------------------------------------------
+fluke_data <- read_tsv(file = "data/01_fluke_data_clean.tsv")
+
+# Adding variables to the data set. Farm and group gets numerical values
+fluke_data_IBM <- fluke_data %>% 
+  mutate(Farm_ID = case_when(Farm == "C1" ~ 1,
+                             Farm == "C2" ~ 2,
+                             Farm == "O1" ~ 3,
+                             Farm == "O2" ~ 4),
+         Group_ID = case_when(Group == "Calf" ~ 1,
+                              Group == "Heifer" ~ 2,
+                              Group == "Primiparous" ~ 3,
+                              Group == "Multiparous" ~ 4)) %>% 
+  select(UniqueID, Farm_ID, Group_ID) %>% 
+  distinct(UniqueID,.keep_all = TRUE)
+
+
+
+
