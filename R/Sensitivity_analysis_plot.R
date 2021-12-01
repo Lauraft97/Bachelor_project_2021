c793012fdfefa7ae61792957124eed84ee996772
SofieSkovgaard <s184329@student.dtu.dk>
2021-12-01 22:39:26 +0100

    scatter plot code updated

--- R/Sensitivity_analysis_plot.R
+++ R/Sensitivity_analysis_plot.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(epiR)
 library(tidyverse)
 library(GGally)
+library(RColorBrewer)
 
 
 load("results/sensitivity_O1_Anna.RData")
@@ -29,8 +30,7 @@ results <- as.data.frame(results)
 
 windowsFonts(`Lucida Bright` = windowsFont("Lucida Bright"))
 color_scheme_2 <- RColorBrewer::brewer.pal(12, "Paired")[1:12]
-
-
+color_scheme <- RColorBrewer::brewer.pal(8, "Set2")[1:8]
 
 
 PRCC <- epi.prcc(results)
@@ -77,21 +77,46 @@ ggsave("results/figures/Final_figures/PRCC.png")
 
 # Scatterplot -------------------------------------------------------------
 
-#Full scatter plot, needs to go into the appendix
-ggpairs(results,  columnLabels = c("\u03BB_ES","\u03BC_Egg","\u03B4_S","\u03B3_S",
-                                   "\u03BC_S","\u03BC_M",
-                                  "Slaughter prob", "egg_\u03BC_scaled",
-                                  "Baseline snail \n population","End infected")) + 
-        theme_bw()
+# ggpairs(results,  columnLabels = c("\u03BB_ES","\u03BC_Egg","\u03B4_S","\u03B3_S",
+#                                    "\u03BC_S","\u03BC_M",
+#                                   "Slaughter prob", "egg_\u03BC_scaled",
+#                                   "Baseline snail \n population","End infected")) + 
+#         theme_bw()
+panel.cor <- function(x, y, digits = 2, cex.cor, ...)
+{
+        usr <- par("usr"); on.exit(par(usr))
+        par(usr = c(0, 1, 0, 1))
+        # correlation coefficient
+        r <- cor(x, y)
+        p <- cor.test(x, y)$p.value
+        txt <- format(c(r, 0.123456789), digits = digits)[1]
+        if(p >= 0.05) txt <- paste0("Corr = \n", txt)
+        if(p < 0.05 & p >= 0.01) txt <- paste0("Corr = \n", txt,"*")
+        if(p < 0.01 & p >= 0.001) txt <- paste0("Corr = \n",txt,"**")
+        if(p < 0.001) txt <- paste0("Corr = \n",txt,"***")
+        text(0.5, 0.5, txt)
+}
 
-#Scatter plot for only significant p-values
-ggpairs(results, columns = c("lambda_ES_sa","mu_Egg_sa","delta_snail_sa","gamma_S_sa","mu_M_sa",
-                             "sla_prob","Snail_pop0","End_infected"),  
-        columnLabels = c("\u03BB_ES","\u03BC_Egg","\u03B4_S","\u03B3_S",
-                         "\u03BC_M",
-                                   "Slaughter prob",
-                                   "Baseline snail \n population","End infected")) + 
-        theme_bw()
 
-ggsave("results/figures/Final_figures/scatter.png")
+# Customize upper panel
+upper.panel<-function(x, y){
+        points(x,y, pch = 16, col = color_scheme[1])
+}
+
+# Create the plots
+pairs(results, 
+      lower.panel = upper.panel,
+      upper.panel = panel.cor,
+      labels = c(expression(lambda[ES]),
+                 expression(mu[Egg]),
+                 expression(delta[S]),
+                 expression(gamma[S]),
+                 expression(mu[S]),
+                 expression(mu[M]),
+                 "Slaughter \n probability",
+                 expression(paste("egg_",mu,"_scaled")),
+                 "Baseline snail \n population",
+                 "End infected"),
+      cex.labels=1.2)
+
 
