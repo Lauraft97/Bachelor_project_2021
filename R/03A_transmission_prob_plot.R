1b2a109db25dee727a36acd5536cc6a00bdbe1b4
Lauraft97 <s184337@student.dtu.dk>
2021-12-04 17:15:12 +0100

    Plots

--- R/03A_transmission_prob_plot.R
+++ R/03A_transmission_prob_plot.R
@@ -3,15 +3,19 @@ rm(list = ls())
 library(tidyverse)
 library(patchwork)
 library(cowplot)
+library(grid)
+library(gridExtra)
 library("RColorBrewer")
 color_scheme <- RColorBrewer::brewer.pal(8, "Set2")[1:8]
 color_scheme_2 <- RColorBrewer::brewer.pal(12, "Paired")[1:12]
+windowsFonts(sans = windowsFont("Lucida Bright"))
+Sys.setlocale("LC_ALL","English")
 
 load(file = "results/sim_transmission.RData")
 
 legend <- sim_transmission %>% ggplot(mapping = aes(x = Visit_name,
                                           y = trans_prob,
-                                          group = Farm_simulation))+
+                                          group = Farm_simulation)) +
   geom_line(data = sim_transmission %>% 
               filter(Farm_simulation %in% c("C1", "C2", "O1", "O2")),
             aes(color = Farm_simulation))+
@@ -31,7 +35,7 @@ legend <- sim_transmission %>% ggplot(mapping = aes(x = Visit_name,
                                                               shape = c(NA,NA,NA,NA,4,4,4)))) +
   theme_bw(base_size = 12)+
   labs(x = "",
-       y = "Transmission probability",
+       y = "",
        title = "Transmission probabilities",
        subtitle = "Comparing simulations with farm data using different scaling factors")+
   theme(legend.position = "bottom")
@@ -58,7 +62,7 @@ p1 <- sim_transmission %>% ggplot(mapping = aes(x = Visit_name,
                                                               shape = c(NA,NA,NA,NA,4,4,4)))) +
   theme_bw(base_size = 12)+
   labs(x = "",
-       y = "Transmission probability",
+       y = "",
        title = "Transmission probabilities",
        subtitle = "Comparing simulations with farm data using different scaling factors")+
   theme(legend.position = "none")
@@ -84,15 +88,20 @@ p2 <- sim_transmission %>% filter(!(Farm_simulation == "simulation_M6")) %>%
                                                               shape = c(NA,NA,NA,NA,4,4)))) +
   theme_bw(base_size = 12)+
   labs(x = "Visit",
-       y = "Transmission probability")+
+       y = "")+
   theme(legend.position = "none")
 
 plot_sim <- plot_grid(p1,p2,nrow = 2)
 plot_legend <- get_legend(legend + theme(legend.justification="bottom"))
 
-plot_grid(plot_sim, plot_legend,
+plot <- plot_grid(plot_sim, plot_legend,
           nrow = 2,
           rel_heights = c(15,1))
 
-ggsave(filename = "results/figures/Final_figures/03_trans_prob.png") 
 
+y.grob <- textGrob("Transmission probability", 
+                   gp=gpar(fontsize=12), rot=90)
+
+print_plot <-grid.arrange(arrangeGrob(plot, left = y.grob))
+
+ggsave(filename = "results/figures/Final_figures/03_trans_prob.png", plot = print_plot) 
